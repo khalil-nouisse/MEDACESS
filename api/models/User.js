d0761f3756c2f0be4a2983a8100d6daac6ef0fd8
tahaprime <bleutahadata@gmail.com>
2025-03-11 22:59:47 +0000

    Updated User Model

--- api/models/User.js
+++ api/models/User.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true},
+    cin: { type: String, required: true, unique: true },
+    sexe: { type: Boolean, required: true},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
+    tel: { type: String, required: true },
+    doti: { type: String }
+    address: { type: String},
+    code_postal: {type: Number},
+    family_statut: {type: String},
 });
 
 module.exports = mongoose.model('User', UserSchema);
