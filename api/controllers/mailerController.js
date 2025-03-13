c6d682be194d8583639e8c630c7f3ac2242f48ef
khalil-nouisse <khalil.ait.nouisse1@gmail.com>
2025-03-13 23:58:33 +0000

    Update mailerController.js

--- api/controllers/mailerController.js
+++ api/controllers/mailerController.js
@@ -11,18 +11,19 @@ const transporter = nodemailer.createTransport({
 
 const sendActivationEmail = (userEmail, activationToken) => {
     const activationLink = `http://${process.env.SERVER}:${process.env.PORT}/auth/activate?key=${activationToken}`;
+
     const mailOptions = {
-        from: `"MedAccess" <${process.env.EMAIL}>`,
+        from: `MedAccess" <${process.env.EMAIL}`,
         to: userEmail,
         subject: "Activate Your Account",
-        html: `
+        html:`
             <h2>Welcome to MedAccess!</h2>
             <p>Click the link below to activate your account:</p>
             <a href="${activationLink}" target="_blank">
                 Activate Account
             </a>
-            <p>If you didn’t sign up for this account, please ignore this email.</p>
-        `,
+            <p>If you didn’t sign up for this account, please ignore this email.</p>`
+        ,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
@@ -35,31 +36,3 @@ const sendActivationEmail = (userEmail, activationToken) => {
 };
 
 module.exports = { sendActivationEmail };
-
-const sendPasswordRecoverEmail = (userEmail, resetToken) => {
-    const resetLink = `http://${process.env.SERVER}:${process.env.PORT}/auth/reset-password?key=${resetToken}`;
-    
-    const mailOptions = {
-        from: `"MedAccess" <${process.env.EMAIL}>`,
-        to: userEmail,
-        subject: "Reset Your Password",
-        html: `
-            <h2>Password Reset Request</h2>
-            <p>Click the link below to reset your password:</p>
-            <a href="${resetLink}" target="_blank">
-                Reset Password
-            </a>
-            <p>This link will expire in 1 hour.</p>
-        `,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error("Error sending email:", error);
-        } else {
-            console.log(`Password recovery email sent to ${userEmail}:`, info.response);
-        }
-    });
-};
-
-module.exports = { sendPasswordRecoverEmail };
